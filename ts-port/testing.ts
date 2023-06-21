fcf4e5702ee745fe5940250e9674208fdfbb06a8
William-TogetherScience <william@together.science>
2023-06-21 16:59:38 -0700

    fixing property assignments

--- ts-port/testing.ts
+++ ts-port/testing.ts
@@ -1,9 +1,5 @@
 import {S} from "./core/singleton.js";
 import {_Number_, Integer} from "./core/numbers.js";
+import {Symbol} from "./core/symbol.js";
 
-const n = _Number_.new(3)
-console.log(n._assumptions)
-console.log(n.is_even())
-console.log(n._assumptions)
-console.log(S.Infinity.is_finite())
-console.log(S.Infinity.is_Pow())
+console.log(new Symbol("x"))
